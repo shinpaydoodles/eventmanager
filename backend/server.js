9f99adbd043d18cc0a062893c86e58d1a5dea97c
Luis Benico <luismiguelbenico@gmail.com>
2024-11-18 11:23:06 +0800

    fix server

--- backend/server.js
+++ backend/server.js
@@ -84,6 +84,6 @@ app.delete('/api/events/:id', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('Server is running on port 5000');
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server is running on port', process.env.PORT || 5000);
 });
