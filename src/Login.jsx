ed7b598b2b76f7507e8ecd0be0cd81e26fdf0461
Luis Benico <luismiguelbenico@gmail.com>
2024-11-18 22:26:25 +0800

    change login

--- src/Login.jsx
+++ src/Login.jsx
@@ -43,60 +43,31 @@ const Login = ({ setIsAuthenticated, setIsAdmin }) => {
   };
 
   return (
-    <div className={`container ${isSignUpMode ? 'sign-up-mode' : ''}`}>
-      <div className="user-admin">
-      <div className='usersignin'>
-        <form onSubmit={handleLogin} className="sign-in-form">
-          <h2 className="title1">User</h2>
-          <div className="input-field">
-            <i className="fas fa-user"></i>
-            <input type="text" name="email" placeholder="Email" onChange={handleInputChange} />
-          </div>
-          <div className="input-field">
-            <i className="fas fa-lock"></i>
-            <input type="password" name="password" placeholder="Password" onChange={handleInputChange} />
-          </div>
-          <input type="submit" className="btn" value="Login" />
-          <p className="account-text">
-            <a href="#" onClick={() => setIsSignUpMode(true)}>Admin</a>
-          </p>
-        </form>
-        </div>
-      <div className='adminsignin'>
-        <form onSubmit={handleLogin} className="sign-up-form">
-          <h2 className="title2">Admin</h2>
-          <div className="input-field">
-            <i className="fas fa-user"></i>
-            <input type="text" name="email" placeholder="Email" onChange={handleInputChange} />
-          </div>
-          <div className="input-field">
-            <i className="fas fa-lock"></i>
-            <input type="password" name="password" placeholder="Password" onChange={handleInputChange} />
-          </div>
-          <input type="submit" className="btn" value="Login" />
-          <p className="account-text">
-            <a href="#" onClick={() => setIsSignUpMode(false)}>User</a>
-          </p>
-        </form>
-        </div>
+    <div className="container">
+  <div className="signin-form">
+    <h2 className="title">Login</h2>
+    <form onSubmit={handleLogin}>
+      <div className="input-field">
+        <i className="fas fa-user"></i>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={handleInputChange}
+        />
       </div>
-
-      <div className="panels-container">
-  <div className="panel left-panel">
-    <div className="content">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnHbRsZ-AWHOSjc-r0hcNEAv3iSyppF4v_Fw&s" alt="User Icon" className="panel-image" />
-      <h5 className="companyname">COLLEGE OF THE IMMACULATE CONCEPTION</h5>
-      <button className="btn" onClick={() => setIsSignUpMode(false)}>User</button>
-    </div>
+      <div className="input-field">
+        <i className="fas fa-lock"></i>
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={handleInputChange}
+        />
+      </div>
+      <input type="submit" className="btn" value="Login" />
+    </form>
   </div>
-  <div className="panel right-panel">
-    <div className="content">
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnHbRsZ-AWHOSjc-r0hcNEAv3iSyppF4v_Fw&s" alt="Admin Icon" className="panel-image" />
-      <h5 className="companyname">COLLEGE OF THE IMMACULATE CONCEPTION</h5>
-      <button className="btn" onClick={() => setIsSignUpMode(true)}>Admin</button>
-    </div>
-  </div>
-</div>
 
       {showModal && (
         <div className="modallogin">
