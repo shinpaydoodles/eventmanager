5905ad8a03b16e0baee5d16687f5504210ae738b
Luis Benico <luismiguelbenico@gmail.com>
2024-11-24 15:08:35 +0800

    calendar

--- src/assets/CalendarAdmin.jsx
+++ src/assets/CalendarAdmin.jsx
@@ -252,7 +252,7 @@ const CalendarAdmin = () => {
     <div className='full-calendar-containeradmin'>
       <div className='calendar-controls'>
         <button onClick={goToPrev} className="back-buttonadmin" id='backbuttonadmin'></button>
-        <span id='calendar-title'>{new Date().toLocaleString('default', { month: 'long' })} 
+        <span id='calendar-title'>{new Date().toLocaleString('default', { month: 'long' })} {''}
           {new Date().getFullYear()}</span>
         <button onClick={goToNext} className="next-buttonadmin" id='nextbuttonadmin'></button>
       </div>
